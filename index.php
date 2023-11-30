76215a45822f3007cfc85fefcde2e7c9a75aacb9
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-30 16:54:11 +0100

    milestone 2 completed

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
 
-if(isset($_GET["password"])) {
-    $password = $_GET["password"];
-    var_dump($password);
-    $characters = 'abcdefghilmnopqrstuvzwyxjk';
-    $charactersCapital = strtoupper($characters);
-    var_dump($charactersCapital);
-    $numbers = '0123456789';
-    $specialCharacters = '~`!@#$%^&*()_-+={[}]|\:;<,>.?/';
-    $allCharacters= $characters . $numbers . $specialCharacters . $charactersCapital;
-    var_dump($allCharacters);
-    $generatedPassword = '';
-    while(strlen($generatedPassword) < $password){
-        $newPassCharacter = $allCharacters[rand(0, strlen($allCharacters) -1)];
-        if(!str_contains($generatedPassword, $newPassCharacter)){
-        $generatedPassword .= $newPassCharacter;
-        }
-    }
-    var_dump($generatedPassword);
-    return $generatedPassword;
-}
+include __DIR__ . './functions/functions.php';
+generatePassword()
 
 ?>
 
